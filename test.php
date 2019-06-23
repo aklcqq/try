0d333ce798cbf9527dfc36e0dafeaa8a85c193e8
aklcqq <48574784+aklcqq@users.noreply.github.com>
2019-06-23 22:54:17 +0800

    Update test.php

--- test.php
+++ test.php
@@ -1,7 +1,6 @@
 <?php
 
 if(/*"chi" == $_POST[username] &&*/ "DaiDai0427" == $_POST[password]){
-if("chi" == $_POST[username] && "chichi" == $_POST[password]){
 
 echo <<<_END
 <!DOCTYPE html>
@@ -56,35 +55,8 @@ echo <<<_END
         width: 90%;}
 
     </style>
-  </head>
-
-  <body>
-    <div class="page">
-    <h1 class="onLeft">Chi.</h1>
-
-    <p> Where <b>I</b>  can read fiction for <i>free</i>.</p>
-
-    <p> Have fun! </p>
-
-    <p> Press<a href="py.php"><b> here</b><a> to refresh.</p>
-    <aside class="list">
-      <ul>
-      <li><a href='nc.html'>fuhan<a></li>
-      <li><a href='zhmy.html'>mengyan<a></li>
-      <li><a href='hxsj.html'>hxsj<a></li>
-      </ul>
-    </aside>
-    </div>
-  </body>
-</html>
 
 _END;
-/*
-  if (file_exists("nc.html")) {
-    echo "file exists";
-  }
-*/
-
 }
 else {
   echo <<<_END
@@ -93,8 +65,6 @@ else {
     <head>
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1">
-
-      <title><b>Welcome, $_POST</b></title>
       <link href=css/s2.css type="text/css" rel="stylesheet" />
       <title>ERROR!</title>
       <style>
@@ -138,10 +108,6 @@ else {
       <p color="red"> WRONG PASSWORD.</p>
     </body>
 </html>
-
-      <p color=red> WRONG USERNAME OR PASSWORD.</p>
-    </body>
-  </html>
 _END;
 }
 ?>
